bb654b32cf35d298fb0a91bdb49aac93f48221af
Jesús Ruiz-Ayúcar <chusete@gmail.com>
2019-11-09 17:13:33 +0100

    Extract a class which handles messages

---
+++ draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/MessageView.java
@@ -0,0 +1,24 @@
+package es.urjccode.mastercloudapps.adcs.draughts.views;
+
+import es.urjccode.mastercloudapps.adcs.draughts.utils.Console;
+
+enum MessageView {
+
+    TITLE("Draughts");
+
+    private String message;
+
+    private static Console console = new Console();
+
+    private MessageView(String message) {
+        this.message = message;
+    }
+
+    public void write() {
+        MessageView.console.write(this.message);
+    }
+
+    public void writeln() {
+        MessageView.console.write(this.message);
+    }
+}
